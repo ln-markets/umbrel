d1eefbadf900c58ba805e13cd071e5ec7bfe11ab
Botrel Kilian <botrel.kilian1999@gmail.com>
2021-10-01 20:39:53 +0200

    fix: env variable name

--- apps/api/entrypoint.sh
+++ apps/api/entrypoint.sh
@@ -3,7 +3,7 @@
 cd front/assets
 
 FILE=`ls | grep 'index.*.js'`
-PORT=`env | grep APP_LNMARKETS_PORT | cut -d '=' -f2`
+PORT=`env | grep API_PORT | cut -d '=' -f2`
 
 sed -i "s/VUE_APP_LNMARKETS_PORT/${PORT}/g" $FILE
 
