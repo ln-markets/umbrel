d10d89e4a1452d602d09a97061f9590e89685db2
Botrel Kilian <botrel.kilian1999@gmail.com>
2022-04-19 16:01:14 +0200

    fix: cors for private ip access (#39)

--- apps/api/src/middleware/cors.js
+++ apps/api/src/middleware/cors.js
@@ -8,6 +8,8 @@ export default cors({
       !origin ||
       origin.match(/^http:\/\/localhost:\d+$/) ||
       origin.match(/^http:\/\/umbrel(.*?).local(:\d+)?$/) ||
+      // eslint-disable-next-line prettier/prettier, no-useless-escape
+      origin.match('/^http://192.168.d{1,3}.d{1,3}(:d+)?$/') ||
       origin.match(`http://${process.env.APP_HIDDEN_SERVICE}`) ||
       origin.match(`http://${process.env.APP_DOMAIN}`)
     ) {
