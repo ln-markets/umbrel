40b0dd3b740dc497eef73953ef57c58fed09d137
Botrel Kilian <botrel.kilian1999@gmail.com>
2022-10-26 17:30:56 +0200

    chore: updates dependencies

--- apps/front/src/components/index.js
+++ apps/front/src/components/index.js
@@ -2,11 +2,8 @@ import Button from './Button.vue'
 import Modal from './Modal.vue'
 import Slider from './Slider.vue'
 
-import FontAwesomeIcon from './font-awesome.js'
-
 export default (app) => {
   app.component('LnmUmbrelButton', Button)
   app.component('LnmUmbrelModal', Modal)
   app.component('LnmUmbrelSlider', Slider)
-  app.component('FontAwesomeIcon', FontAwesomeIcon)
 }
